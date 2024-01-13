3bed8e74734d390e6aa0883cff3024551ad9b055
Jefferson Aguiar <jefferson.aguiar@facens.br>
2024-01-13 18:42:23 -0300

    chore: systemset and ordering

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,22 +1,30 @@
 use systems::*;
 
-use bevy::app::{App, Plugin, Startup, Update};
+use bevy::prelude::*;
 
 pub mod components;
 mod systems;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player).add_systems(
-            Update,
-            (
-                player_movement,
-                confine_player_movement,
-                enemy_hit_player,
-                player_hit_star,
-            ),
-        );
+        app.configure_sets(Update, MovementSystemSet.before(ConfinementSystemSet))
+            .add_systems(Startup, spawn_player)
+            .add_systems(
+                Update,
+                (
+                    player_movement.in_set(MovementSystemSet),
+                    confine_player_movement.in_set(ConfinementSystemSet),
+                    enemy_hit_player,
+                    player_hit_star,
+                ),
+            );
     }
 }
