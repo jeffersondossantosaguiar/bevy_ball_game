876976a122c24d0f3567e8eb620e6e91cf087542
Jefferson Aguiar <jefferson.aguiar@facens.br>
2024-01-20 22:02:12 -0300

    feat: Bevy UI, main menu

--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -1,13 +1,24 @@
 use bevy::prelude::*;
 
+use systems::layout::*;
+
+use crate::main_menu::systems::interactions::*;
+use crate::AppState;
+
+mod components;
+mod styles;
+mod systems;
+
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, main_menu);
+        app.add_systems(OnEnter(AppState::MainMenu), spawn_main_menu)
+            .add_systems(
+                Update,
+                (interact_with_play_button, interact_with_quit_button)
+                    .run_if(in_state(AppState::MainMenu)),
+            )
+            .add_systems(OnExit(AppState::MainMenu), despawn_main_menu);
     }
-}
-
-pub fn main_menu() {
-    println!("You are on the main menu.");
 }
